b3fb0912ccbdf81bc6e0e8012abfc2239ecae2d2
KyleDolce <tan_kai_jie@soc.uum.edu.my>
2024-11-26 17:40:28 +0800

    Notes to add on

--- AssignmentOne.java
+++ AssignmentOne.java
@@ -1,14 +1,17 @@
 public class AssignmentOne {
+    //private variables
     private int carManufacturedYear;
     private double carPrice;
     private String carName;
 
+    //constructor
     public AssignmentOne(int carManufacturedYear, double carPrice, String carName) {
         this.carManufacturedYear = carManufacturedYear;
         this.carPrice = carPrice;
         this.carName = carName;
     }
 
+    //method to later display the car details also calculation
     public void displayCarDetails() {
         double carCalculation = carPrice*carManufacturedYear;
         System.out.println("Car Name: " + carName);
@@ -17,6 +20,7 @@ public class AssignmentOne {
         System.out.println("Car Calculation: RM" + carCalculation);
     }
     
+    //main method to send value then call method to display
     public static void main(String[] args) {
         AssignmentOne car = new AssignmentOne(2023, 168888, "Nissan Leaf");
         car.displayCarDetails();

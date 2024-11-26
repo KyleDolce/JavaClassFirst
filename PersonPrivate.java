c6a86c71925dec988bbf589770a2b700ed971639
KyleDolce <tan_kai_jie@soc.uum.edu.my>
2024-11-26 17:31:02 +0800

    Assignment one

--- PersonPrivate.java
+++ PersonPrivate.java
@@ -1,9 +1,11 @@
 public class PersonPrivate {
+    //private variables
     private String name;
     private int age;
     private double height;
     private double weight;
 
+    //constructor
     public PersonPrivate(String name, int age, double height, double weight) {
         this.name = name;
         this.age = age;
@@ -11,13 +13,17 @@ public class PersonPrivate {
         this.weight = weight;
     }
 
+    //method to display details
     public void displayDetails() {
+        double bmi = weight / (height * height);
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Height: " + height +"m");
         System.out.println("Weight: " + weight +"kg");
+        System.out.println("BMI: " + bmi);
     }
 
+    //main method
     public static void main(String[] args) {
         PersonPrivate person = new PersonPrivate("Tan Kai Jie", 22, 1.65, 65);
         person.displayDetails();

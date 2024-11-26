5b3ca151815fe529bae6e5c7495bffeabe68ed9e
KyleDolce <tan_kai_jie@soc.uum.edu.my>
2024-11-26 17:15:33 +0800

    modification

--- Person.java
+++ Person.java
@@ -1,14 +1,17 @@
 public class Person {
-    
+    String name = "Tan Kai Jie";
+    int age = 22;
+    double height = 1.65;
+
+    public void displayPerson(){
+        System.out.println("My name is : " + name);
+        System.out.println("My age is : " + age);
+        System.out.println("My height is " +height + "m");
+    }
 
     public static void main(String[] args) {
-        String name = "Tan Kai Jie";
-        int age = 22;
-        boolean isStudent = true;
-        double height = 1.65;
-        long netWorth = 99999999999L;
-        System.out.println("The student name is : " + name + " and age is: " +age);
-        System.out.println("The student height is : " + height + " and is a student: " +isStudent);
-        System.out.println("The student net worth is:RM " +netWorth );
+        Person person = new Person();
+        person.displayPerson();
+        
     }
 }

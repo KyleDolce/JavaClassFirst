528de408e08dbfa085472f47265a63aab20e93a6
KyleDolce <tan_kai_jie@soc.uum.edu.my>
2024-11-26 16:53:18 +0800

    Object Oriented Version

--- VariableScope.java
+++ VariableScope.java
@@ -1,16 +1,17 @@
 public class VariableScope {
     
-    int instanceVariable =10;
+    int instanceVariable =22;
 
-    static int staticVariable = 20;
+    static double staticVariable = 1.65;
 
     public void showLocalVariable(){
-        int localVariable =30;
-        System.out.println("Local Variable: "+localVariable);
-        System.out.println("instance Variable: " +instanceVariable);
-        System.out.println("static Variable: "+staticVariable);
+        String localVariable ="Tan Kai Jie";
+        System.out.println("Name: "+localVariable);
+        System.out.println("Age: " +instanceVariable);
+        System.out.println("height: "+staticVariable);
 
     }
+    
 
     public static void main(String[] args) {
         VariableScope variableScope = new VariableScope();
